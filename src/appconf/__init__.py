ef55ae1a9b349524c35638547320f1fe644b8655
Martin Smith <msmith@bitmonster.org>
2026-03-10 21:04:41 -0500

    Version Bump

--- src/appconf/__init__.py
+++ src/appconf/__init__.py
@@ -5,7 +5,7 @@ from .bind import Bind, BindDefault
 from .app_config import AppConfig
 from .providers import ConfigProvider, BackingStore, DefaultedValue, ArgParseWrapper, ArgNamespaceProvider, ArgParseProvider, OmegaConfProvider
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 
 __all__ = [
     "OmegaConfig",
